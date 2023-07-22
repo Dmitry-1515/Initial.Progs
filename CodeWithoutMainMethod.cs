85e1b821e7191812e1e625373bd0ea3708786bf9
Dmitry-1515 <132833839+Dmitry-1515@users.noreply.github.com>
2023-07-22 10:39:37 +0300

    Add files via upload

---
+++ CodeWithoutMainMethod.cs
@@ -0,0 +1,50 @@
+﻿namespace Practice
+{
+    // Create class Student and print object parameters
+
+    // Создаём класс Student со своими полями
+
+    class Student
+    {
+        public string Name;
+    }
+
+    class Program
+    {
+        // Записываем метод для присвоения объекту значения 
+        static Student GetStudent()
+        {
+            // Объявляем объект класса Student
+
+            var student = new Student();
+
+            // Присваиваем полю объекта значение
+
+            student.Name = "Степан";
+
+            // Возвращаем объект student
+
+            return student;
+        }
+
+        // Записываем метод для печати значения объекта 
+        static void Print(Student student)
+        {
+            // Печать значений объекта
+
+            Console.WriteLine(student.Name);
+        }
+        static void Main()
+        {
+            // Объявляем объект класса Student
+
+            var firstStudent = GetStudent();
+
+            // Печатаем данные объекта
+
+            Print(firstStudent);
+        }
+
+    }
+
+}
