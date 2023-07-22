d68e1765ccae6f1f81f78dcdefe3e8a2a2d31c10
Dmitry-1515 <132833839+Dmitry-1515@users.noreply.github.com>
2023-07-22 11:06:32 +0300

    Add files via upload

---
+++ CompareNumbers.cs
@@ -0,0 +1,28 @@
+﻿// Input two numbers and compare them
+
+string data = Console.ReadLine();
+
+string data1 = Console.ReadLine();
+
+int a = int.Parse(data);
+
+int b = int.Parse(data1);
+
+if (a == b)
+
+    {
+        Console.WriteLine("a = b");
+    }
+
+if (a > b)
+
+    {
+        Console.WriteLine("a > b");
+    }
+
+if (a < b)
+
+    {
+        Console.WriteLine("a < b");
+    }
+
